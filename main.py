5f89de8c9744accf43906b6b319a4ca516526287
Sam Banankhu Mhango <sambanankhumhango@gmail.com>
2022-06-28 23:47:27 +0200

    fourth commit

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
 import pandas as pd
 import pdfplumber
+from tqdm import tqdm
+import time
 
 pdfPath = r"C:\Users\pandas\Pictures\Certificates\World Countries and Capital & Languages.pdf"
-pdf = pdfplumber.open(pdfPath)
-# print(pdf.metadata)
-# print(pdf.pages)
-# print(pdf.pages[0].extract_table()[1::])
 
-pages = pdf.pages.extract_table()
 
-country = []
-capital = []
-currency = []
-primaryLang = []
+def __get_pdf_path__(path):
+    pdf = pdfplumber.open(pdfPath)
+    # print(pdf.metadata)
+    # print(pdf.pages)
+    # print(pdf.pages[0].extract_table()[1::])
 
-for j in range(len(pages)):
-    for i in range(len(pages[j].extract_table())):
-        country.append(pages[j].extract_table()[i][0])
-        capital.append(pages[j].extract_table()[i][1])
-        currency.append(pages[j].extract_table()[i][-2])
-        primaryLang.append(pages[j].extract_table()[i][-1])
+    pages = pdf.pages.extract_table()
+
+    return pages
+
+
+def __extract_pages__(pages):
+    country = []
+    capital = []
+    currency = []
+    primaryLang = []
+
+    for j in range(len(pages)):
+        for i in range(len(pages[j].extract_table())):
+            country.append(pages[j].extract_table()[i][0])
+            capital.append(pages[j].extract_table()[i][1])
+            currency.append(pages[j].extract_table()[i][-2])
+            primaryLang.append(pages[j].extract_table()[i][-1])
+
+    return country, capital, currency, primaryLang
+
+pdf_dict = {
+    'country':[],
+    'capital':[],
+    'currency':[],
+    'primaryLang':[]
+}
+
+def __create_repository__(country, capital, currency, primaryLang):
