d1b77d1058269edc1fe5ceb30fddf49ae17e3e03
Sam Banankhu Mhango <sambanankhumhango@gmail.com>
2022-06-28 22:31:24 +0200

    Second commit

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ import pandas as pd
 import pdfplumber
 
 pdfPath = r"C:\Users\pandas\Pictures\Certificates\World Countries and Capital & Languages.pdf"
-with pdfplumber.open(pdfPath) as pdf:
-    print(pdf.metadata)
-    print(pdf.pages)
+pdf = pdfplumber.open(pdfPath)
+# print(pdf.metadata)
+# print(pdf.pages)
+# print(pdf.pages[0].extract_table()[1::])
+
+pages = pdf.pages.extract_table()
+
+country = []
+capital = []
+currency = []
+primaryLang = []
+
+for j in range(len(pages)):
+    for i in range(len(pages[j].extract_table())):
+        country.append(pages[j].extract_table()[i][0])
+        capital.append(pages[j].extract_table()[i][1])
+        currency.append(pages[j].extract_table()[i][-2])
+        primaryLang.append(pages[j].extract_table()[i][-1])
