748bab7baca20353b739f20896ca2bc0d9a8abda
Sam Banankhu Mhango <sambanankhumhango@gmail.com>
2022-06-28 22:24:18 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import pdfplumber
+
+pdfPath = r"C:\Users\pandas\Pictures\Certificates\World Countries and Capital & Languages.pdf"
+with pdfplumber.open(pdfPath) as pdf:
+    print(pdf.metadata)
+    print(pdf.pages)
